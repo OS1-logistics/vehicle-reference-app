2ac7cdc9fa6f69a93feb7d3b3681fa9db7c3b1f7
kshitijsharmadelhivery <kshitij.sharma@delhivery.com>
2024-01-29 23:29:42 +0530

    AB#19401 changed local main.ts file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  app.useStaticAssets('public', {
+    prefix: '/static', // Adjust this prefix as needed
+    setHeaders: (res, path) => {
+      if (path.endsWith('.js')) {
+        res.setHeader('Content-Type', 'application/javascript');
+      }
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
